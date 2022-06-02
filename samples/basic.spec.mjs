9d161ca9b9537b20532103f357dda9bccb0a8116
imbrn <brunodev02221@gmail.com>
2022-06-02 12:54:12 -0300

    samples test

--- samples/basic.spec.mjs
+++ samples/basic.spec.mjs
@@ -7,11 +7,19 @@ globalThis.stuff = Orchestrator.create([], {
 });
 
 globalThis.stuff.observers.add({
-  onStateChange: () => console.log('state changed'),
+  onStateChange: (state) =>
+    console.log(`state changed: ${JSON.stringify(state, null, 2)}`),
 });
 
+console.log(globalThis.stuff.state);
+
 globalThis.stuff.state.theme = 'light';
+console.log(globalThis.stuff.state.theme);
+
+globalThis.stuff.state.user = { token: '123456' };
+console.log(globalThis.stuff.state.user);
+
+globalThis.stuff.state.language = 'en-US';
+console.log(globalThis.stuff.state.language);
 
 console.log(globalThis.stuff.state);
-console.log(globalThis.stuff.state.theme);
-console.log(globalThis.stuff.state.user);
