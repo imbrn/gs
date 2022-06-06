68c76113746cd7f5566afb33b48194979a5925b4
imbrn <brunodev02221@gmail.com>
2022-06-06 17:40:29 -0300

    tests messages feedback improved

--- tests/test-samples.ts
+++ tests/test-samples.ts
@@ -1,45 +1,45 @@
-import assert from 'assert';
-import { execSync } from 'child_process';
-import * as fs from 'fs';
-import path from 'path';
+import assert from "assert";
+import { execSync } from "child_process";
+import * as fs from "fs";
+import path from "path";
 
 (async function () {
   const node = process.argv0;
   const projecRootPath = getProjectRootPath();
-  const samplesPath = path.join(projecRootPath, 'samples');
+  const samplesPath = path.join(projecRootPath, "samples");
 
   fs.readdirSync(samplesPath)
-    .filter((testFileName) => testFileName.endsWith('.spec.mjs'))
+    .filter((testFileName) => testFileName.endsWith(".spec.mjs"))
     .forEach((testFileName) => {
-      const testName = testFileName.split('.')[0];
+      const testName = testFileName.split(".")[0];
 
       const expectedOutputName = `${testName}.output.txt`;
       const expectedOutputFile = path.join(
         samplesPath,
-        `${expectedOutputName}`,
+        `${expectedOutputName}`
       );
       const expectedOutput = fs
         .readFileSync(expectedOutputFile, {
-          encoding: 'utf8',
+          encoding: "utf8",
         })
-        .replace(/\r\n/g, '\n');
+        .replace(/\r\n/g, "\n");
 
       const specPath = path.join(samplesPath, testFileName);
 
       const output = execSync(`${node} ${specPath}`, {
-        cwd: path.join(projecRootPath, 'tests'),
-        encoding: 'utf8',
-      }).replace(/\r\n/g, '\n');
+        cwd: path.join(projecRootPath, "tests"),
+        encoding: "utf8",
+      }).replace(/\r\n/g, "\n");
 
       try {
         assert.equal(output, expectedOutput);
-        console.info(`${testName} passed successfuly.`);
+        console.info(`Test "${testName}" passed successfuly.`);
       } catch (error) {
         console.error(error);
         console.error(`Assert error for file: ${testName}`);
-        console.error('Expected:');
+        console.error("Expected:");
         console.info(expectedOutput);
-        console.error('Received:');
+        console.error("Received:");
         console.info(output);
       }
     });
